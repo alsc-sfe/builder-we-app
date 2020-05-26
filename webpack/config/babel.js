9ef06902c87d3117619989d755953d10ba4efa8b
zetay.lim <zetay.lim@alibaba-inc.com>
2020-05-26 16:54:14 +0800

    fix: react css module 禁用

--- webpack/config/babel.js
+++ webpack/config/babel.js
@@ -1,5 +1,23 @@
 const { resolve } = require;
-const { CSS_SCOPE } = require('../util/const');
+const get = require('lodash/get');
+const { CSS_SCOPE, SAAS_CONFIG } = require('../util/const');
+
+const useReactCssModule = get(SAAS_CONFIG, 'useReactCssModule', true);
+const pluginsReactCssModule = useReactCssModule ? [
+  resolve('@saasfe/babel-plugin-react-css-module-transformer'),
+  [resolve('babel-plugin-react-css-modules'), {
+    context: 'src',
+    generateScopedName: `${CSS_SCOPE}_[path][local]_[hash:base64:5]`,
+    webpackHotModuleReloading: true,
+    handleMissingStyleName: 'warn',
+    filetypes: {
+      ".less": {
+        "syntax": "postcss-less"
+      }
+    },
+    autoResolveMultipleImports: true
+  }],
+] : [];
 
 module.exports = {
   loader: resolve('babel-loader'),
@@ -43,19 +61,7 @@ module.exports = {
         libraryDirectory: 'es',
         style: true,
       }, 'choice-cbm'],
-      resolve('@saasfe/babel-plugin-react-css-module-transformer'),
-      [resolve('babel-plugin-react-css-modules'), {
-        context: 'src',
-        generateScopedName: `${CSS_SCOPE}_[path][local]_[hash:base64:5]`,
-        webpackHotModuleReloading: true,
-        handleMissingStyleName: 'warn',
-        filetypes: {
-          ".less": {
-            "syntax": "postcss-less"
-          }
-        },
-        autoResolveMultipleImports: true
-      }]
+      ...pluginsReactCssModule,
     ],
   }
 }
